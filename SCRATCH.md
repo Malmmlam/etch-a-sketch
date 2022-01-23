a4c19ffcbffbf53b18049884f4be5d6bee0f326a
Josh <malmmlam@outlook.com>
2022-01-23 16:27:27 +0100

    add reset and change grid size functionality

--- SCRATCH.md
+++ SCRATCH.md
@@ -1,7 +1,13 @@
 Plan
     Restate
         Make a 16x16 grid of squares. Relationship between the squares and the grid size must remain the same regardless of the size of the squares. 
+        Add functionality so that user can enter desired grid size
     Reduce
         Make a 4x4 grid of squares. 
             create 2 rows. Add 2 cells to each row.
         Make an 8x8 grid of squares.
+        Add grid size functionality
+            ask user for a number.
+            This number is then used as the limiting factor when creating the grid. As long as the i and y variables used to create the grid are less than the user entered number the program will continue to add cells to the grid. 
+        Remove previous grid before adding new grid.
+            remove all children of container. Get all rows. for each row remove the row from container.
--- js/script.js
+++ js/script.js
@@ -1,22 +1,57 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('button');
+button.addEventListener('click', reset);
+let gridSize = 16;
+play(gridSize);
+
+function play (gridSize) {
+    drawGrid(gridSize);
+    getUserShading();
+}
 
 function shadeCell (cell) {
     this.classList.add('shaded');
 }
 
-for(let i = 1; i <= 16; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
+function reset () {
+    gridSize = getGridSize(gridSize);
+    clearGrid();
+    play(gridSize);
+}
 
-    for(let y = 1; y <= 16; y++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
+function getGridSize (gridSize){
+    gridSize = parseInt(prompt('Please enter a grid size', 16));
+    if(gridSize > 100 || gridSize < 1) {
+        alert('Grid must be bigger than 0 and smaller than 100!');
+        getGridSize(gridSize);
+    }
+    return gridSize;
+}
+
+function clearGrid () {
+    let rows = document.querySelectorAll('.row');
+    rows.forEach(row => container.removeChild(row));
+}
+
+function drawGrid (gridSize) {
+    for(let i = 1; i <= gridSize; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+    
+        for(let y = 1; y <= gridSize; y++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
     }
 }
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => cell.addEventListener('mouseover', shadeCell));
+function getUserShading () {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.addEventListener('mouseover', shadeCell));
+}
+
+
 
 
